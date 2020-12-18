ef72a55637c47a45fa6793b4c003f6e04526bf76
Ivan Bosch <ivan.bosch.amengual@gmail.com>
2020-12-18 16:19:16 +0100

    Update

--- src/DiesEntreDates_Iván_Bosch.java
+++ src/DiesEntreDates_Iván_Bosch.java
@@ -8,54 +8,89 @@ public class DiesEntreDates_Iván_Bosch extends CalcularDiesEntreDates{
     //Builder
     public DiesEntreDates_Iván_Bosch() {
     }
+    int diesMesInicial;
+    int diesMesDesti;
+    int diesMes;
+    int diesResteAnyInicial;
+    int diesResteAnyDesti;
+    int diesNumAnysComplets;
+    boolean anyDeTraspas;
 
-    //Imported methods
     @Override
     protected int diesMes(int mes) {
-        return 0;
+        switch (mes) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                this.diesMes = 31;
+                break;
+            case 2:
+                this.diesMes=28;
+                break;
+            case 4: case 6: case 9: case 11:
+                this.diesMes = 30;
+                break;
+        }
+        return this.diesMes;
     }
 
     @Override
     protected int diesMesInicial(DataXS dataXS) {
-        return 0;
+        this.diesMesInicial = diesMes(dataXS.mes) - dataXS.dia;
+        return this.diesMesInicial;
     }
 
     @Override
     protected int diesMesDesti(DataXS dataXS) {
-        return 0;
+        this.diesMesDesti = dataXS.dia;
+        return this.diesMesDesti;
     }
 
     @Override
     protected int diesResteAnyInicial(DataXS datainicial) {
-        return 0;
+        datainicial.mes++;
+        while (datainicial.mes <= 12){
+            this.diesResteAnyInicial += diesMes(datainicial.mes);
+            datainicial.mes++;
+        }
+        return this.diesResteAnyInicial;
     }
 
     @Override
     protected int diesResteAnyDesti(DataXS datadesti) {
-        return 0;
+        datadesti.mes--;
+        while (datadesti.mes >= 1){
+            this.diesResteAnyDesti += diesMes(datadesti.mes);
+            datadesti.mes--;
+        }
+        return this.diesResteAnyDesti;
     }
 
     @Override
     protected int diesNumAnysComplets(DataXS datainicial, DataXS datadesti) {
-        return 0;
+        this.diesNumAnysComplets = ((datadesti.any - datainicial.any) -1) * 365;
+        return this.diesNumAnysComplets;
     }
 
     @Override
     protected int numDiesPerAnysdeTraspas(DataXS datainicial, DataXS datadesti) {
-        return 0;
+        int diaTraspas = 0;
+        for (int i = 0; i <= ((datadesti.any - datainicial.any) -1); i++) {
+            if (anyDeTraspas(datainicial.any + i)){
+                diaTraspas++;
+            }
+        }
+        if (anyDeTraspas(datadesti.any)){
+            diaTraspas++;
+        }
+        return diaTraspas;
     }
 
     @Override
     protected boolean anyDeTraspas(int any) {
-        return false;
-    }
+        if (any % 400 == 0 || ((any % 4 == 0) && !(any % 100 == 0))){
+            this.anyDeTraspas = true;
+        }
+        else this.anyDeTraspas = false;
 
-    public final int nombreDiesTotals(DataXS datainicial, DataXS datadesti) {
-        return this.diesMesInicial(datainicial)+
-                this.diesResteAnyInicial(datainicial)+
-                this.diesNumAnysComplets(datainicial,datadesti)+
-                this.diesResteAnyDesti(datadesti)+
-                this.diesMesDesti(datadesti)+
-                this.numDiesPerAnysdeTraspas(datainicial,datadesti);
+        return this.anyDeTraspas;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,15 @@
 public class Main {
     public static void main(String[] args) {
         DiesEntreDates_Iván_Bosch d1=new DiesEntreDates_Iván_Bosch();
-        System.out.println(d1.nombreDiesTotals());
+        try {
+            DataXS dataInici=new DataXS("11/02/2012");
+            DataXS dataFinal=new DataXS("12/02/2013");
+            System.out.println("There is a "+d1.nombreDiesTotals(dataInici,dataFinal)+" day differential including both days.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
     }
 
 
